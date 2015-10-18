3308fe4c8bdeee4d417067178ddd3ac2a77d7b46
bpandola <brian.pandola@philips.com>
2015-10-18 04:00:26 -0700

    Only need to declare state structures for implemented object states

--- js/actstate.js
+++ js/actstate.js
@@ -1,100 +1,84 @@
-﻿(function () {
-    var ST_INFO_NULL = [0, FvB.SPR_DEMO, 0, null, null, FvB.ST_DEAD]
-
+﻿(function () {    
     /*
-        1-if object can be rotated, 0 if one sprite for every direction
-        base object's state texture if rotation is on facing player
-        after how man frames change state to .next_state
-        what to do every frame
-        what to do once per state
-        next state
+        Object State Structures
+
+        State: st_ index constant
+        Rotate: 1-if object can be rotated left/right, 0 if one sprite for every direction
+        Texture: sprite image or base lef-facing image for rotatable sprites 
+        Timeout: how many frames until state change or 0 if state will change externally
+        Think:  what to do every frame
+        Action: what to do once per state
+        NextState: entity state after Timeout
     */
 
-    //var objstate[NUM_OBJECTS][NUM_STATES] = [
-    var objstate = [
-        // Fartsac
-        [
-            [1, FvB.SPR_FARTSAC_STAND_1, 0, FvB.Player.T_Stand, null, FvB.Player.T_Stand],
-            [1, FvB.SPR_FARTSAC_CROUCH_1, 0, FvB.Player.T_Crouch, null, FvB.Player.T_Crouch],
-            [1, FvB.SPR_FARTSAC_JUMP_1, 0, FvB.Player.T_Jump, null, FvB.Player.T_Jump],
-            [1, FvB.SPR_FARTSAC_JUMP_1, 0, FvB.Player.T_Jump, null, FvB.Player.T_Jump],
-            ST_INFO_NULL,
-            [1, FvB.SPR_FART_BLOW_1, 0, FvB.Player.T_Blow, null, FvB.Player.T_Stand]
-        ],
-        // Boogerboy
-        [
-            [1, FvB.SPR_BOOGERBOY_STAND_1, 0, FvB.Player.T_Stand, null, FvB.Player.T_Stand],
-            [1, FvB.SPR_BOOGERBOY_CROUCH_1, 0, FvB.Player.T_Crouch, null, FvB.Player.T_Crouch],
-            [1, FvB.SPR_BOOGERBOY_JUMP_1, 0, FvB.Player.T_Jump, null, FvB.Player.T_Jump],
-            [1, FvB.SPR_BOOGERBOY_JUMP_1, 0, FvB.Player.T_Jump, null, FvB.Player.T_Jump],
-            ST_INFO_NULL,
-            [1, FvB.SPR_BOOG_BLOW_1, 0, FvB.Player.T_Blow, null, FvB.Player.T_Stand]
-        ],
-        // Fartball
-        [
-            ST_INFO_NULL, 
-            ST_INFO_NULL,
-            ST_INFO_NULL,
-            ST_INFO_NULL,
-            [0, FvB.SPR_FARTBALL, 0, FvB.AI.T_Projectile, null, FvB.AI.T_Projectile]
-        ],
-        // Booger
-        [
-            ST_INFO_NULL,
-            ST_INFO_NULL,
-            ST_INFO_NULL,
-            ST_INFO_NULL,
-            [0, FvB.SPR_BOOGER, 0, FvB.AI.T_Projectile, null, FvB.AI.T_Projectile]
-        ],
-        // Huge Fartball
-        [
-            ST_INFO_NULL,
-            ST_INFO_NULL,
-            ST_INFO_NULL,
-            ST_INFO_NULL,
-            [1, FvB.SPR_HUGE_FARTBALL, 0, FvB.AI.T_Projectile, null, FvB.AI.T_Projectile]
-        ],
-        // Huge Booger
-        [
-            ST_INFO_NULL,
-            ST_INFO_NULL,
-            ST_INFO_NULL,
-            ST_INFO_NULL,
-            [1, FvB.SPR_HUGE_BOOGER, 0, FvB.AI.T_Projectile, null, FvB.AI.T_Projectile]
-        ],
-        // Explosion
-        [
-            [0, FvB.SPR_EXPLOSION_0, 0, FvB.AI.T_Explode, null, FvB.ST_DEAD],
-        ]
-
-
-
+    var objStateData = [
+       // en_Fartsac
+       [
+           [FvB.st_Stand, 1, FvB.SPR_FARTSAC_STAND_1, 0, FvB.Player.T_Stand, null, FvB.st_Stand],
+           [FvB.st_Crouch, 1, FvB.SPR_FARTSAC_CROUCH_1, 0, FvB.Player.T_Crouch, null, FvB.st_Stand],
+           [FvB.st_JumpUp, 1,FvB.SPR_FARTSAC_JUMP_1, 0, FvB.Player.T_Jump, null, FvB.st_JumpDown],
+           [FvB.st_JumpDown,1, FvB.SPR_FARTSAC_JUMP_1, 0, FvB.Player.T_Jump, null, FvB.st_Stand],
+           [FvB.st_Blow,1, FvB.SPR_FART_BLOW_1, 0, FvB.Player.T_Blow, null, FvB.st_Stand],
+           [FvB.st_Damaged, 1, FvB.SPR_FARTSAC_DAMAGED, 50, null, null, FvB.st_Stand],
+           [FvB.st_NearlyDead, 1, FvB.SPR_FARTSAC_STAND_1, 0, null, null, FvB.st_Dead],
+           [FvB.st_Dead, 1, FvB.SPR_FARTSAC_STAND_1, 0, null, null, FvB.st_Dead]
+       ],
+       // en_Boogerboy
+       [
+           [FvB.st_Stand, 1, FvB.SPR_BOOGERBOY_STAND_1, 0, FvB.Player.T_Stand, null, FvB.st_Stand],
+           [FvB.st_Crouch, 1, FvB.SPR_BOOGERBOY_CROUCH_1, 0, FvB.Player.T_Crouch, null, FvB.st_Stand],
+           [FvB.st_JumpUp, 1, FvB.SPR_BOOGERBOY_JUMP_1, 0, FvB.Player.T_Jump, null, FvB.st_JumpDown],
+           [FvB.st_JumpDown, 1, FvB.SPR_BOOGERBOY_JUMP_1, 0, FvB.Player.T_Jump, null, FvB.st_Stand],
+           [FvB.st_Blow, 1, FvB.SPR_BOOG_BLOW_1, 0, FvB.Player.T_Blow, null, FvB.st_Stand],
+           [FvB.st_Damaged, 1, FvB.SPR_BOOGERBOY_DAMAGED, 50, null, null, FvB.st_Stand],
+           [FvB.st_NearlyDead, 1, FvB.SPR_BOOGERBOY_STAND_1, 0, null, null, FvB.st_Dead],
+           [FvB.st_Dead, 1, FvB.SPR_BOOGERBOY_STAND_1, 0, null, null, FvB.st_Dead]
+       ],
+       // en_Fartball
+       [
+           [FvB.st_Path, 0, FvB.SPR_FARTBALL, 0, FvB.AI.T_Projectile, null, FvB.st_Path]
+       ],
+       // en_Booger
+       [
+           [FvB.st_Path, 0, FvB.SPR_BOOGER, 0, FvB.AI.T_Projectile, null, FvB.st_Path]
+       ],
+       // en_HugeFartball
+       [
+           [FvB.st_Path, 1, FvB.SPR_HUGE_FARTBALL, 0, FvB.AI.T_Projectile, null, FvB.st_Path]
+       ],
+       // en_HugeBooger
+       [
+           [FvB.st_Path,1, FvB.SPR_HUGE_BOOGER, 0, FvB.AI.T_Projectile, null,  FvB.st_Path]
+       ],
+       // en_Explosion
+       [
+           [FvB.st_StaticOnce, 0, FvB.SPR_EXPLOSION_0, 0, FvB.AI.T_Explode, null, FvB.st_Remove],
+       ]
     ];
+    
+    //
+    // Convert objStateData to final form: objstate[NUM_ENTITY_TYPES][NUM_ENTITY_STATES]
+    // 
+    var ST_INFO_NULL = [0, FvB.SPR_DONT_USE, 0, null, null, FvB.st_Remove];
 
-    /*  
-    typedef struct
-    {
-        char rotate; // 1-if object can be rotated, 0 if one sprite for every direction
-        int texture; // base object's state texture if rotation is on facing player
-        int timeout; // after how man ticks change state to .next_state
-        think_t think; // what to do every frame
-        think_t action; // what to do once per state
-        en_state next_state; // next state
-    } 
-    */
-
-    // convert to state structs
-    for (var i = 0; i < objstate.length; i++) {
-        var obj = objstate[i];
-        for (var j = 0; j < obj.length; j++) {
+    var objstate = [[]];
+    for (var i = 0; i < objStateData.length; i++) {
+        // First init all states to ST_INFO_NULL
+        objstate[i] = [];
+        for (var j = 0; j <= FvB.st_Remove; j++) {
+            objstate[i][j] = ST_INFO_NULL;
+        }
+        // Now fill in all the states we have defined in objStateData
+        var obj = objStateData[i];
+        for (var j = 0; j < obj.length; j++) {            
             var state = obj[j];
-            obj[j] = {
-                rotate: state[0],
-                texture: state[1],
-                timeout: state[2],
-                think: state[3],
-                action: state[4],
-                next_state: state[5]
+            objstate[i][state[0]] = {
+                rotate: state[1],
+                texture: state[2],
+                timeout: state[3],
+                think: state[4],
+                action: state[5],
+                next_state: state[6]
             };
         }
     }
