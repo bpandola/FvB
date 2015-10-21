d8eea034a3760deb645fc4aa95ec696ba8ad8c01
bpandola <brian.pandola@philips.com>
2015-10-21 02:54:59 -0700

    Added loader to preload gfx and sfx and display progress bar on title screen

---
+++ js/load.js
@@ -0,0 +1,60 @@
+﻿(function ($) {
+
+    $(document).ready(function () {
+
+        var progress = $("<div>");
+
+        progress.addClass("load-progress").appendTo("#title-screen");
+        $("#title-screen").show();
+       
+        // Entering callback hell...
+        loadGraphics();
+
+        function loadGraphics() {
+            progress.text("Loading graphics");
+            FvB.Sprites.init();
+
+            var checkGfx = setInterval(function () {
+                progress.text(progress.text() + ".");
+                if (!FvB.Sprites.isReady())
+                    return;
+
+                clearInterval(checkGfx);
+                loadSounds();
+
+            }, 1000);
+        }
+
+        function loadSounds() {
+            progress.text("Loading sounds");
+            FvB.Sound.init(FvB.soundResources);
+
+            var checkSounds = setInterval(function () {
+                progress.text(progress.text() + ".");
+                if (!FvB.Sound.finishedLoading())
+                    return;
+
+                clearInterval(checkSounds);
+                finishedLoading();
+
+            }, 1000);
+        }
+        
+        function finishedLoading() {
+            progress.text("Ready.  Click anywhere to run.");
+            document.getElementById('title-screen').addEventListener('click', function () {
+
+
+                $("#title-screen").fadeOut(1500, function () {
+                    //Wolf.Input.init();
+                    //Wolf.Game.init();
+                    //Wolf.Menu.show();
+                    
+                    FvB.Game.startGame();
+                });
+            });
+        }
+       
+    });
+
+})(jQuery);
