be718b631d5de50f46f12937be4fdc4a323f1df8
bpandola <brian.pandola@philips.com>
2015-10-21 02:51:43 -0700

    Put requestAnimationFrame polyfill in its own js file

---
+++ js/requestanimframe.js
@@ -0,0 +1,24 @@
+﻿/* requestAnimationFrame polyfill */
+(function () {
+
+    window.requestAnimFrame = (function () {
+        return window.requestAnimationFrame
+			|| window.webkitRequestAnimationFrame
+			|| window.mozRequestAnimationFrame
+			|| window.oRequestAnimationFrame
+			|| window.msRequestAnimationFrame
+			|| function (callback) {
+			    window.setTimeout(callback, 1000 / 60);
+			};
+    })();
+
+    window.cancelRequestAnimFrame = (function () {
+        return window.cancelRequestAnimationFrame
+			|| window.webkitCancelRequestAnimationFrame
+			|| window.mozCancelRequestAnimationFrame
+			|| window.oCancelRequestAnimationFrame
+			|| window.msCancelRequestAnimationFrame
+			|| window.clearTimeout;
+    })();
+
+})();
