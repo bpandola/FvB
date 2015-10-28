b18f05a2f64757d44551e746f3cfa229ff097339
bpandola <brian.pandola@philips.com>
2015-10-27 22:00:41 -0700

    New music and sfx

--- js/sounds.js
+++ js/sounds.js
@@ -11,22 +11,26 @@
         { constant: "SFX_SLURP", sfx: "slurp" },
         { constant: "SFX_BULLET", sfx: "smallexplodey" },
         { constant: "SFX_SPLAT", sfx: "splat" },
-        { constant: "SFX_RYU_THEME", sfx: "ryu-theme" },
         { constant: "SFX_SELECT_CHARACTER", sfx: "select_character" },
         { constant: "SFX_CHOOSE_CHARACTER", sfx: "choose_character" },
         { constant: "SFX_ROUND1", sfx: "round1" },
         { constant: "SFX_ROUND2", sfx: "round2" },
         { constant: "SFX_ROUND3", sfx: "round3" },
         { constant: "SFX_FIGHT", sfx: "fight" },
-        { constant: "SFX_CHARACTER_SELECT_MUSIC", sfx: "Street Fighter II Arcade Music - Character Select - CPS1" },
-         { constant: "SFX_FARTSAC_THEME", sfx: "Street Fighter II Arcade Music - Ken Stage - CPS1" },
-    { constant: "SFX_BOOGERBOY_THEME", sfx: "Street Fighter II Arcade Music - Guile Stage - CPS1" },
-    { constant: "SFX_TITLE_THEME", sfx: "Street Fighter II Arcade Music - Opening Theme - CPS1" }
+        { constant: "SFX_TITLE_THEME", sfx: "Street Fighter II Arcade Music - Opening Theme - CPS1" },
+        { constant: "SFX_CHARACTER_SELECT_THEME", sfx: "Street Fighter II Arcade Music - Character Select - CPS1" },
+        { constant: "SFX_FARTSAC_THEME", sfx: "Street Fighter II Arcade Music - Ken Stage - CPS1" },
+        { constant: "SFX_BOOGERBOY_THEME", sfx: "Street Fighter II Arcade Music - Guile Stage - CPS1" },
+        { constant: "SFX_YOHAN_THEME", sfx: "Street Fighter II Arcade Music - Zangief Stage - CPS1" },
+        { constant: "SFX_RYU_THEME", sfx: "ryu-theme" },
+        { constant: "SFX_VERSUS_THEME", sfx: "9BH" },
+{ constant: "SFX_YOU_WIN", sfx: "youwin" },
+{ constant: "SFX_YOU_LOSE", sfx: "youlose" }
     ];
 
     var soundConsts = {};
     for (var i = 0, n = sfxMappings.length; i < n; i++) {
-        soundConsts[sfxMappings[i].constant] = sfxMappings[i].sfx;
+        soundConsts[sfxMappings[i].constant] = i; //sfxMappings[i].sfx;
     }
     FvB.setConsts(soundConsts);
 
